30a2e2178bb89726912604de3dc4017db7297ae6
DavidTu <a3102a123@gamil.com>
2020-11-21 13:04:44 +0000

    fix pi syntax error

--- pi_block_linear.cc
+++ pi_block_linear.cc
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <math.h>
+#include <algorithm>
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -19,7 +22,8 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
     long long int range = tosses / world_size , begin_idx = 0,begin,end; 
     static unsigned int seed = time(NULL) + world_rank;
-    long long int x,y,distance_squared;
+    long long int x,y;
+    double distance_squared;
     begin = range * world_rank;
     end = min(range * (world_rank + 1) , tosses);
     long long int count = 0;
@@ -33,20 +37,22 @@ int main(int argc, char **argv)
         x = rand();
         y = rand();
         distance_squared = x * x + y * y;
+        printf("%lf ",distance_squared);
         if ( distance_squared <= 1)
             count++;
     }
 
     if (world_rank > 0)
     {
-        MPI_Send(count ,1 ,MPI_LONG_LONG,dst ,0 ,MPI_COMM_WORLD );
+        MPI_Send(&count ,1 ,MPI_LONG_LONG,dst ,0 ,MPI_COMM_WORLD );
     }
     else if (world_rank == 0)
     {
         MPI_Status status; 
         long long int temp;
         for(int source = 1 ; source < world_size ; source++){
-            MPI_Recv(temp, 1, MPI_LONG_LONG, source, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&temp, 1, MPI_LONG_LONG, source, 0, MPI_COMM_WORLD, &status);
+            printf("%lld\n",temp);
             count += temp;
         }
         // TODO: master
