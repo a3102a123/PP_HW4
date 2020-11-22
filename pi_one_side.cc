788deafb02ec977fb5579c939fa8eb83b5bc9432
DavidTu <a3102a123@gamil.com>
2020-11-22 16:42:46 +0800

    add one side pi

--- pi_one_side.cc
+++ pi_one_side.cc
@@ -4,6 +4,38 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+double inv_max = 2.0 / RAND_MAX;
+
+int fnz (int *schedule, int *oldschedule, int size, long long int *count){
+    int diff = 0;
+
+    for (int i = 0; i < size; i++)
+        diff |= (schedule[i] != oldschedule[i]);
+
+    if (diff)
+    {
+        int res = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            if(schedule[i] != oldschedule[i]){
+                printf("\t%d", schedule[i]);
+                (*count) += schedule[i];
+            }
+            res += 1;
+            oldschedule[i] = schedule[i];
+        }
+        printf("\n");
+
+        return(res == size-1);
+    }
+    return 0;
+
+}
 
 int main(int argc, char **argv)
 {
@@ -18,14 +50,59 @@ int main(int argc, char **argv)
     MPI_Win win;
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
+    long long int range = ceil(tosses / (float)world_size) , begin_idx = 0,begin,end; 
+    static unsigned int seed = time(NULL) * world_rank;
+    double distance_squared,x,y;
+    long long int count = 0;
+    int root = 0;
+
+    begin = range * world_rank;
+    end = min(range * (world_rank + 1) , tosses);
+
+    for(int i = begin ; i < end ; i++){
+        x = (double)rand() * inv_max  + (-1.0);
+        y = (double)rand() * inv_max  + (-1.0);
+        distance_squared = x * x + y * y;
+        if ( distance_squared <= 1)
+            count++;
+    }
 
     if (world_rank == 0)
     {
         // Master
+        long long int *oldschedule = malloc(world_size * sizeof(long long int));
+        long long int *schedule;
+        MPI_Alloc_mem(world_size * sizeof(long long int), MPI_INFO_NULL, &schedule);
+        for (int i = 0; i < world_size; i++)
+        {
+            schedule[i] = 0;
+            oldschedule[i] = -1;
+        }
+        MPI_Win_create(schedule, world_size * sizeof(long long int), sizeof(long long int), MPI_INFO_NULL,MPI_COMM_WORLD, &win);
+        int ready = 0;
+        while (!ready)
+        {
+            MPI_Win_lock(MPI_LOCK_SHARED, 0, 0, win);
+            ready = fnz(schedule, oldschedule, world_size);
+            MPI_Win_unlock(0, win);
+        }
+
+        MPI_Win_free(&win);
+        MPI_Free_mem(schedule);
+        free(oldschedule);
     }
     else
     {
         // Workers
+        MPI_Win_create(NULL, 0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, 0, win);
+        MPI_Put(&count, 1, MPI_LONG_LONG, 0, world_rank, 1, MPI_LONG_LONG, win);
+        MPI_Win_unlock(0, win);
+
+        MPI_Win_free(&win);
     }
 
     MPI_Win_free(&win);
@@ -33,7 +110,7 @@ int main(int argc, char **argv)
     if (world_rank == 0)
     {
         // TODO: handle PI result
-
+        pi_result = 4.0 * (double)count /(( double ) tosses);
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
