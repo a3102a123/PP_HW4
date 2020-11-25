8a6f86e1b6a107a2226be12d02171c38015570bd
DavidTu <a3102a123@gamil.com>
2020-11-25 06:00:22 +0000

    add rank size in fun

--- myMatrix.cc
+++ myMatrix.cc
@@ -1,15 +1,22 @@
 #include<bits/stdc++.h>
-int world_rank, world_size;
-MPI_Comm_size(MPI_COMM_WORLD,&world_size);
-MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
+#include <mpi.h>
 void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,int **a_mat_ptr, int **b_mat_ptr){
+    int world_rank, world_size;
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
     printf("Construct Matrix %d\n",world_rank);
 }
 
 void matrix_multiply(const int n, const int m, const int l,const int *a_mat, const int *b_mat){
+    int world_rank, world_size;
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
     printf("Calctulate Matrix\n");
 }
 
 void destruct_matrices(int *a_mat, int *b_mat){
+    int world_rank, world_size;
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
     printf("Destroy Matrix\n");
 }
