818c73c84421702c7fd8d73bb939874c35a0b2a4
DavidTu <a3102a123@gamil.com>
2020-11-25 13:36:24 +0800

    add makefile & correlated c++ file

---
+++ myMatrix.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,int **a_mat_ptr, int **b_mat_ptr){
+    printf("Construct Matrix\n");
+}
+
+void matrix_multiply(const int n, const int m, const int l,const int *a_mat, const int *b_mat){
+    printf("Calctulate Matrix\n");
+}
+
+void destruct_matrices(int *a_mat, int *b_mat){
+    printf("Destroy Matrix\n");
+}
