160718373aa5d89e6142a886d8c21e3df495d173
DavidTu <a3102a123@gamil.com>
2020-11-25 13:50:43 +0800

    test mpi

--- myMatrix.cc
+++ myMatrix.cc
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
+int world_rank, world_size;
+MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
 void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,int **a_mat_ptr, int **b_mat_ptr){
-    printf("Construct Matrix\n");
+    printf("Construct Matrix %d\n",world_rank);
 }
 
 void matrix_multiply(const int n, const int m, const int l,const int *a_mat, const int *b_mat){
