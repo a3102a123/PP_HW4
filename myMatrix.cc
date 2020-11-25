61237ed64cf81b953709b4d851a2e7fd3417e4d4
DavidTu <a3102a123@gamil.com>
2020-11-25 14:33:01 +0800

    read input in matrix

--- myMatrix.cc
+++ myMatrix.cc
@@ -5,22 +5,27 @@ void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,int **a_mat_ptr, int 
     MPI_Comm_size(MPI_COMM_WORLD,&world_size);
     MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
     printf("Construct Matrix %d\n",world_rank);
-    if (world_rank > 0)
+    /*only MPI_COMM_WORLD rank 0 inherits stdin*/
+    if (world_rank == 0)
     {
         scanf("%d %d %d",n_ptr,m_ptr,l_ptr);
+        a_mat_ptr = (*int) malloc( sizeof(int) * (*n_ptr) * (*m_ptr));
+        a_mat_ptr = (*int) malloc( sizeof(int) * (*m_ptr) * (*l_ptr));
+        for(int i = 0 ; i < n_ptr ; i++)
+            for(int j = 0 ; j < m_ptr ; j++)
+                scanf("%d",a_mat_ptr + i*(*m_ptr) + j);
+        for(int i = 0 ; i < m_ptr ; i++)
+            for(int j = 0 ; j < l_ptr ; j++)
+                scanf("%d",b_mat_ptr + i*(*l_ptr) + j);
     }
-    else if (world_rank == 0)
-    {
-        scanf("%d %d %d",n_ptr,m_ptr,l_ptr);
-    }
-    printf("%d %d %d\n",*n_ptr,*m_ptr,*l_ptr);
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 void matrix_multiply(const int n, const int m, const int l,const int *a_mat, const int *b_mat){
     int world_rank, world_size;
     MPI_Comm_size(MPI_COMM_WORLD,&world_size);
     MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
-    printf("Calctulate Matrix\n");
+    printf("Calctulate Matrix in %d : %d\n",world_rank,*a_mat);
     if (world_rank > 0)
     {
     }
